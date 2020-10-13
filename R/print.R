203d5180e796f4bee460193583871c29f07b9b2a
Naraa Altai <naltai@wesleyan.edu>
2020-10-13 15:50:56 -0400

    add print, plot, and summary

---
+++ R/print.R
@@ -0,0 +1,27 @@
+#' Print one-way ANOVA results
+#'
+#' print.oneway prints one-way ANOVA results.
+#'
+#' @param x an object of class oneway.
+#' @param ... additional arguments passed to the function.
+#'
+#' @return the input object is returned silently.
+#' @export
+#' @examples
+#' # mileage <- oneway(mpg~cyl, mtcars)
+#' print(mileage)
+print.oneway <- function(x, ...){
+
+  ## check if x is oneway object
+  if(!inherits(x, "oneway"))
+    stop("Must be class 'oneway'")
+
+
+  cat("Summary Statistics \n", "==============================\n")
+  print(x$summarystats, ...)
+  cat("Anova \n",  "==============================\n")
+  print(summary.lm(x$anova))
+
+  ## more to fix: blank before the ======
+  ## space after the titles
+}
